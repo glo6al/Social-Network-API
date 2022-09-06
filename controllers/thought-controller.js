4adc44ab8c54ac8b5afab5621bfd0d611b3337da
glo6al <towers.a@gmail.com>
2022-09-06 12:14:01 -0400

    add date format

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,7 +1,15 @@
 const { User, Thought } = require("../models");
 
 const thoughtController = {
-  getAllThoughts(req, res) {},
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .select("-__v")
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
   getThoughtById(req, res) {},
   addThought(req, res) {},
   updateThought(req, res) {},
